c8c440a59cbc71043cf430c6b1b9cdfe5d10c40e
liangliang <liangliang@icourt.cc>
2018-09-17 15:58:37 +0800

    first commit

---
+++ claudine-core/src/main/java/com/us/claudine/core/ClaudineCoreApplication.java
@@ -0,0 +1,12 @@
+package com.us.claudine.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaudineCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaudineCoreApplication.class, args);
+	}
+}
---
+++ claudine-redis/src/main/java/com/us/claudine/redis/ClaudineRedisApplication.java
@@ -0,0 +1,12 @@
+package com.us.claudine.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaudineRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaudineRedisApplication.class, args);
+	}
+}
