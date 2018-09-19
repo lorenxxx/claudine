01272e233f92dd798931924e696e78d499fbfa54
梁良 <liangliang@icourt.cc>
2018-09-19 18:04:48 +0800

    feat: 工程目录调整

---
+++ claudine-common/src/main/java/com/us/claudine/common/util/DateUtil.java
@@ -0,0 +1,12 @@
+package com.us.claudine.common.util;
+
+/**
+ * Created by Loren on 19/09/2018.
+ */
+public class DateUtil {
+
+    public static void excute() {
+        System.out.println("hello claudine");
+    }
+
+}
