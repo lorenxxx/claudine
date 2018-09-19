4354d0b2b2a6778c57aaf36a5cc594b8a9f88536
梁良 <liangliang@icourt.cc>
2018-09-19 19:19:55 +0800

    others: pom文件格式化

--- claudine-common/src/main/java/com/us/claudine/common/util/DateUtil.java
+++ claudine-common/src/main/java/com/us/claudine/common/util/DateUtil.java
@@ -1,12 +1,20 @@
 package com.us.claudine.common.util;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by Loren on 19/09/2018.
  */
 public class DateUtil {
 
-    public static void excute() {
-        System.out.println("hello claudine");
+    /**
+     * 获取当前时间
+     *
+     * @return 当前时间
+     */
+    public static Date now() {
+        return Calendar.getInstance().getTime();
     }
 
 }
